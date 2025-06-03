11363a98e5fd57744a863e66c85455ee1023d532
SirTee <ojotaye33@gmail.com>
2025-06-03 15:08:11 +0100

    Updated HG-front

--- HG-front/src/Components/Hero/Hero.jsx
+++ HG-front/src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="hero-left">
         <h1>PROFESSIONAL MAKEUP ARTIST</h1>
         <h2>Thank you for choosing</h2>
-        <h1 className="glams">HAPPY_GLAMS BEAUTY</h1>
+        <h1 className="glams">HAPPY GLAMS BEAUTY</h1>
         <div className='hero-desc'>
           Customer satisfaction is our ultimate priority, keep patronizing us and we will  keep improving our services to make your day whole.
         </div>
